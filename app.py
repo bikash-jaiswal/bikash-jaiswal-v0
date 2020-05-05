a784378bfda64069d8425ee923afe2d1b07175c6
Bikash Jaiswal <bjjaiswal@gmail.com>
2020-05-05 16:19:30 -0400

    added base, blog +2 files

--- app.py
+++ app.py
@@ -6,8 +6,17 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/home')
+def home():
+    return render_template('home.html',title = 'Home')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html',title = 'Blog')
+
+@app.route('/project')
+def project():
+    return render_template('project.html',title = 'Projects')
 
 if __name__ == '__main__':
-    app.run()    
+    app.run(debug = True)    
